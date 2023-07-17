93ce3f969cd8f04cf6ff72cf4d1523c9bac8dcac
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2023-07-17 12:28:32 +0200

    ♻️ Refactored code + 📝 added documentation

--- src/components/header/LanguageSwitch.jsx
+++ src/components/header/LanguageSwitch.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import LanguageContext from '../../context/LanguageContext';
 
 const LanguageSwitch = () => {
-    const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
+  const { language, changeLanguage } = useContext(LanguageContext);
 
+  /**
+   * Function to handle the language change event.
+   * It updates the language state, changes the language in the translation context,
+   * and stores the selected language in local storage.
+   */
+  const handleLanguageChange = (event) => {
+    const selectedLanguage = event.target.value;
 
-    const handleLanguageChange = (event) => {
-        const selectedLanguage = event.target.value;
-        i18n.changeLanguage(selectedLanguage);
-        localStorage.setItem('language', selectedLanguage); // Save selected language
-      };
-  
-    return (
-      <div>
-        <select onChange={handleLanguageChange} defaultValue={i18n.language}>
-          <option value="en">English</option>
-          <option value="fr">Français</option>
-        </select>
-      </div>
-    );
-}
+    i18n.changeLanguage(selectedLanguage);
+    changeLanguage(selectedLanguage);
+    localStorage.setItem('language', selectedLanguage); // Save selected language
+  };
+
+  return (
+    <div>
+      <select onChange={handleLanguageChange} value={language}>
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+      </select>
+    </div>
+  );
+};
 
 export default LanguageSwitch;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './utils/i18n';
 
 import './assets/styles/css/index.css'
 import App from './App';
@@ -10,10 +8,8 @@ import { LanguageProvider } from './context/LanguageContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </I18nextProvider>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
