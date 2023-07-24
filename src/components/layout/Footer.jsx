13e576ac9ffd3d687ef096f9c1266f342e6469a5
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2023-07-24 20:03:11 +0200

    ✨ Added Footer structure.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
+    const { t } = useTranslation();
+
     return (
-        <div className=''>
-            <p>Footer</p>
-        </div>
+        <footer className='footer'>
+            <div className='footer--links'>
+                <div>
+                    <span><NavLink to="/">Viktoriia Zaichuk</NavLink></span>
+                    <div>
+                        <NavLink to="/about">
+                            {t('navbar.about')}
+                        </NavLink>
+                        <NavLink to="/projects">
+                            {t('navbar.projects')}
+                        </NavLink>
+                        <NavLink to="/contact">
+                            {t('navbar.contact')}
+                        </NavLink>
+                    </div>
+                </div>
+                <div>
+                    <div>Contact me</div>
+                    <div>
+                        <a>LinkedIn</a>
+                        <a>Github</a>
+                        <a>Design portfolio</a>
+                        <a>Codepen</a>
+                    </div>
+                </div>
+            </div>
+            <div className='footer--legal'>
+                <span>©2023 Viktoriia Zaichuk</span>
+            </div>
+        </footer>
     )
 }
 
