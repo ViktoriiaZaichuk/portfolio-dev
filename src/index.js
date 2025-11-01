5ed79caab1585a1f1a9a468a2f269213ff6240fa
Vika Zaichuk <viktoriia.zaichuk@gmail.com>
2025-11-01 08:50:43 +0100

    🐛 fix url for gihub pages

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './assets/styles/css/index.css';
 import App from './App';
 import { LanguageProvider } from './context/LanguageContext';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './context/ThemeProvider';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-dev">
+    <HashRouter>
       <LanguageProvider>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
